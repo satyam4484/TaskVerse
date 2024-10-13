6f76b45ce6d3b8cedc7386a883555fad5c76a55b
satyam <satyambsingh93@gmail.com>
2024-10-13 17:40:06 +0530

    dashboards

--- client/src/Components/Forms/AuthForm.tsx
+++ client/src/Components/Forms/AuthForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const AuthForm: React.FC<{ formType: string }> = ({ formType }) => {
